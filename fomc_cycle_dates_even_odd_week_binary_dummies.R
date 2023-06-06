40293f011c1a2ac9539156a048fb036b7f411b74
Felix Reichel <f.reichel@gmx.at>
2023-06-06 18:47:18 +0200

    minor cleanup

--- fomc_cycle_dates_even_odd_week_binary_dummies.R
+++ fomc_cycle_dates_even_odd_week_binary_dummies.R
@@ -1,29 +1,15 @@
 # Functions
-
-#is_odd_or_even_date_in_fomc_cycle <- function(fomc_start_date, date) {
-#  diff_weeks_numeric <- difftime(date, fomc_start_date, units = "weeks")
-#  diff_weeks_integer <- floor(diff_weeks_numeric)
-#  if ( as.integer(diff_weeks_integer) %% 2 == 0 ) { # return(as.integer(diff_weeks_integer) %% 2 == 0) 
-#    return(1)  
-#  } else {
-#    return(0)  
-#  }
-# = TRUE/1 for days in even week within the FOMC Cycle counting from 0. (0,2,4,6)
-# = FALSE/0 for days in odd week within the FOMC Cycle. (1,3,5)
-# }
-
 is_date_in_t_floor_week_within_fomc_cycle <- function(fomc_start_date, date, t_floor_week) {
   diff_weeks_numeric <- difftime(date, fomc_start_date, units = "weeks")
   diff_weeks_integer <- floor(diff_weeks_numeric)
   if ( diff_weeks_integer == t_floor_week ) {
-    return(1)
+    return(1) 
   } else {
     return(0)
   }
 }
 
-
-is_date_in_ge_7_floor_week_within_fomc_cycle <- function(fomc_start_date, date) {
+is_date_in_greater_or_equal_7_floor_week_within_fomc_cycle <- function(fomc_start_date, date) {
   diff_weeks_numeric <- difftime(date, fomc_start_date, units = "weeks")
   diff_weeks_integer <- floor(diff_weeks_numeric)
   if ( diff_weeks_integer >= 7 ) {
@@ -47,13 +33,11 @@ fomc_data <- read_excel(
   col_types = c("date", "date", "text"),
   skip = 10)
 
-fomc_start_dates <- rev(fomc_data$Startdate) # reverse fomc start dates
+fomc_start_dates <- rev(fomc_data$Startdate) # Reverse FOMC start dates in .xlsx File
 
 # create empty vectors
 dates <- c()
-# is_in_even_fomc_week <- c()
-
-# 7 week dummies
+# 7 FOMC cycle week dummies
 w_t0 <- c()
 w_t1 <- c()
 w_t2 <- c()
@@ -84,16 +68,9 @@ for (next_fomc_start_date in remaining_fomc_start_dates) {
   for (date in days_between_fomc_meetings_seq) {
 
     date <- as.Date(date, origin = lubridate::origin)
-      
-    # in_even_or_odd_week <- is_odd_or_even_date_in_fomc_cycle(
-    #                                                        prev_fomc_start_date, 
-    #                                                        date)
-    
-    
     dates <- c(dates, date)
-    # is_in_even_fomc_week <- c(is_in_even_fomc_week, in_even_or_odd_week)
     
-    # generate week dummies
+    # generate FOMC cycle week dummies
     is_date_in_0_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 0)
     is_date_in_1_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 1)
     is_date_in_2_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 2)
@@ -101,7 +78,7 @@ for (next_fomc_start_date in remaining_fomc_start_dates) {
     is_date_in_4_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 4)
     is_date_in_5_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 5)
     is_date_in_6_week <- is_date_in_t_floor_week_within_fomc_cycle(prev_fomc_start_date, date, 6)
-    is_date_in_ge7_week <- is_date_in_ge_7_floor_week_within_fomc_cycle(prev_fomc_start_date, date)
+    is_date_in_ge7_week <- is_date_in_greater_or_equal_7_floor_week_within_fomc_cycle(prev_fomc_start_date, date)
     
     # append dummies to vectors
     w_t0 <- c(w_t0, is_date_in_0_week)
@@ -118,48 +95,9 @@ for (next_fomc_start_date in remaining_fomc_start_dates) {
 }
 
 # Create dataframe 
-# with Ex-ante (shifted t-x weeks) dummies where x of integer set {0,1,2,3,4,5,6}.
-# dummies_len <- length(is_in_even_fomc_week)
 week_len <- 7
 
-# Testing stored vector dates
-# as.Date(dates[1], origin = lubridate::origin)
-# [1] "2018-01-30"
-# as.Date(dates[(dummies_len - (week_len * 7))], origin = lubridate::origin)
-# "2023-01-30"
-# as.Date(dates[dummies_len], origin = lubridate::origin)
-# "2023-03-20"
-# as.Date(dates[dummies_len+1], origin = lubridate::origin)
-# [1] NA
-
-
 df <- data.frame(
-  # date = as.Date(dates[1 : (dummies_len - (week_len * 7)) ], origin = lubridate::origin),
-  # dummy0 = is_in_even_fomc_week[1 : (dummies_len - (week_len * 7)) ],
-  # dummy1 = is_in_even_fomc_week[((week_len * 1) + 1) : (dummies_len - (week_len * 6))],
-  # dummy2 = is_in_even_fomc_week[((week_len * 2) + 1) : (dummies_len - (week_len * 5))],
-  # dummy3 = is_in_even_fomc_week[((week_len * 3) + 1) : (dummies_len - (week_len * 4))],
-  # dummy4 = is_in_even_fomc_week[((week_len * 4) + 1) : (dummies_len - (week_len * 3))],
-  # dummy5 = is_in_even_fomc_week[((week_len * 5) + 1) : (dummies_len - (week_len * 2))],
-  # dummy6 = is_in_even_fomc_week[((week_len * 6) + 1) : (dummies_len - (week_len * 1))],
-  # dummy7 = is_in_even_fomc_week[((week_len * 7) + 1) :  dummies_len]
-
-  date = as.Date(dates, origin = lubridate::origin),
-  # 7 week dummies for approx. 6.5 ordinary FOMC cycle time.
-  w_t0 = w_t0, # even
-  w_t1 = w_t1, 
-  w_t2 = w_t2, # even
-  w_t3 = w_t3,
-  w_t4 = w_t4, # even
-  w_t5 = w_t5,
-  w_t6 = w_t6,  # even
-  # 8th dummy variable for unordinary FOMC cycle times
-  w_ge_t7 = w_tge7
-)
-
-
-
-df2 <- data.frame(
   date = as.Date(dates, origin = lubridate::origin),
   w_t0 = w_t6,
   w_t1 = w_t5, 
@@ -170,13 +108,6 @@ df2 <- data.frame(
   w_t6 = w_t0
 )
 
-
 # Write csv containing FOMC odd/even week dummies
-write.csv(df2, 
-          'data/dates_is_in_even_fomc_week_dummies.csv', 
-          row.names = FALSE
-          )
-
-
-
+write.csv(df, 'data/dates_is_in_even_fomc_week_dummies.csv', row.names = FALSE)
 
--- sp500_data_scraping.R
+++ sp500_data_scraping.R
@@ -8,42 +8,20 @@ setwd(dirname(current_path))
 getSymbols.FRED(c("SP500"), auto.assign = TRUE, env = globalenv())
 
 # Jfyi: Data misses holidays and weekends
-
 xts18mar23 <- SP500["2018-01-30/2023-03-20"]
 
-
-# Clean Data
 # Omit NA values
-length(xts18mar23) # [1] 1340
-
 xts18mar23 <- na.omit(xts18mar23)
 
-length(xts18mar23) # [1] 1293
-
-# Fill missing values using last observation
-# xts_last <- na.locf(xts2)
-
-# Fill missing values using next observation
-# xts_last <- na.locf(xts2,  fromLast=TRUE)
-
-sp500_x <- index(xts18mar23)
-
-sp500_y <- as.numeric(xts18mar23)
-
-
-# test plot S&P 500
-plot(sp500_y, type = 'l', lwd=2)
-plot(xts18mar23, main = "S&P 500")
-
+# plot
+plot(xts18mar23, main = "S&P500")
 
 # Create data.frame
 sp500_df <- data.frame(
-  date = sp500_x,
-  sp500_y = sp500_y
+  date = index(xts18mar23),
+  sp500_y = as.numeric(xts18mar23)
 )
 
 # Write csv
 write.csv(sp500_df, 'data/sp500_df.csv', row.names = FALSE)
-
-
 
