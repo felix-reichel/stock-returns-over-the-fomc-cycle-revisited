cffb6079e3e3cbfa6cedf266cf7c9e50c1ce6376
Felix Reichel <f.reichel@gmx.at>
2023-06-06 17:03:13 +0200

    S&P500 data scraping

--- sp500_data_scraping.R
+++ sp500_data_scraping.R
@@ -1,24 +1,46 @@
-install.packages("pacman")
-install.packages("tidyquant")
-library(pacman)
-library(tidyquant)
-p_load(tidyquant)
-
-tq_index("SP500") # ^GSPC
-# sp500_ticker <- '^GSPC'
-# tq_get(sp500_ticker, get, from = "1995-01-01")
-# Could not establish session after 5 attempts.
-
-
 require(quantmod)
+require(xts)
 
-getSymbols.google(c("^GSPC"), auto.assign = TRUE, from = "1995-01-01")
-# Error: ‘getSymbols.google’ is defunct.
-# Google Finance stopped providing data in March, 2018.
+# Get S&P500 from 2018-01-30 to 2023-03-20
+getSymbols.FRED(c("SP500"), auto.assign = TRUE, env = globalenv())
 
-getSymbols.yahoo(c("SP500"), auto.assign = TRUE, from = "1995-01-01")
-getSymbols.yahoo(c("SP500"), auto.assign = TRUE, from = "1995-01-01", env = globalenv())
-# Error in new.session() : Could not establish session after 5 attempts.
+# Jfyi: Data misses holidays and weekends
 
-getSymbols.FRED(c("SP500"), auto.assign = TRUE, from = "1995-01-01", env = globalenv())
-plot(SP500) # xts object with last 10 years of s&p 500
+xts18mar23 <- SP500["2018-01-30/2023-03-20"]
+
+
+# Clean Data
+# Omit NA values
+length(xts18mar23) # [1] 1340
+
+xts18mar23 <- na.omit(xts18mar23)
+
+length(xts18mar23) # [1] 1293
+
+# Fill missing values using last observation
+# xts_last <- na.locf(xts2)
+
+# Fill missing values using next observation
+# xts_last <- na.locf(xts2,  fromLast=TRUE)
+
+sp500_x <- index(xts18mar23)
+
+sp500_y <- as.numeric(xts18mar23)
+
+
+# test plot S&P 500
+plot(sp500_y, type = 'l', lwd=2)
+plot(xts18mar23, main = "S&P 500")
+
+
+# Create data.frame
+sp500_df <- data.frame(
+  sp500_x = sp500_x,
+  sp500_y = sp500_y
+)
+
+# Write csv
+write.csv(sp500_df, 'data/sp500_df.csv', row.names = FALSE)
+
+
+
